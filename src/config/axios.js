6d63d98886ab07805f4aa69c4089e75ef4334ce4
Agustin-morcillo <morcilloagustin95@gmail.com>
2021-06-29 23:08:30 -0300

    production url

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const axiosClient = axios.create({
   baseURL:
     process.env.REACT_APP_API_URL ||
-    "https://products-crud-redux.vercel.app/api/",
+    "https://redux-product-crud.herokuapp.com/api/",
 })
 
 export default axiosClient
